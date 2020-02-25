0b54f8aa91b8675efd9218b251d1d0213fb1324b
Mohammad Kiani <kianimohammad@outlook.com>
2020-02-25 03:52:03 -0500

    change color, correct a bug

--- app/src/main/java/com/f19/tictacdemo/MainActivity.java
+++ app/src/main/java/com/f19/tictacdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.f19.tictacdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -11,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     /*
-    * 0 is player yellow
-    * 1 is player red
+    * 0 is player yellow or nought
+    * 1 is player red or cross
     * */
     private int player = 0;
 
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     /*
     * winning positions
     * */
-    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}};
+    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
 
     // boolean value to check the game
     boolean isActive = true;
@@ -36,10 +37,12 @@ public class MainActivity extends AppCompatActivity {
             gameState[state] = player;
             imageView.setTranslationY(-2000);
             if (player == 0) {
-                imageView.setImageResource(R.drawable.yellow);
+//                imageView.setImageResource(R.drawable.yellow);
+                imageView.setImageResource(R.drawable.nought);
                 player = 1;
             } else {
-                imageView.setImageResource(R.drawable.red);
+//                imageView.setImageResource(R.drawable.red);
+                imageView.setImageResource(R.drawable.cross);
                 player = 0;
             }
 
@@ -50,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
                         gameState[winningPosition[0]] != -1) {
                     // we have a winner
                     isActive = false;
-                    String winner = (player == 0) ? "Red" : "Yellow";
+//                    String winner = (player == 0) ? "Red" : "Yellow";
+                    String winner = (player == 0) ? "Cross" : "Nought";
                     Toast.makeText(this, "The winner is " + winner, Toast.LENGTH_SHORT).show();
 
                 }
